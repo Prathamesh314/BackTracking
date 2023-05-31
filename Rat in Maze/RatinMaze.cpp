4ce9256f09aee0ac87a2c0ab68050dcfe7161f5a
Prathamesh314 <124865398+Prathamesh314@users.noreply.github.com>
2023-05-31 16:36:02 +0530

    Updated main files

--- Rat in Maze/RatinMaze.cpp
+++ Rat in Maze/RatinMaze.cpp
@@ -1,6 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+// Here we are checking whether mouse can travel on given path or there is a wall
 bool isSafe(int** arr,int row,int col,int n){
     if(row<n && col<n && arr[row][col] == 1){
         return true;
@@ -8,28 +9,38 @@ bool isSafe(int** arr,int row,int col,int n){
     return false;
 }
 
+// Here we are finding an escape path for mouse
 bool travel(int** arr,int row,int col,int n,int** solArr){
 
+
+    // If rat is able to reach at the last position of the matrix then he has escaped the maze and we return true
     if(row==n-1 && col == n-1){
         solArr[row][col] = 1;
         return true;
     }
 
+    // Here we are checking first whether there is path or wall
     if(isSafe(arr,row,col,n)){
-        solArr[row][col] = 1;
-        if(travel(arr,row+1,col,n,solArr)){
-            return true;
+        solArr[row][col] = 1; // These is our assumption that mouse can escape out by following this path
+        if(travel(arr,row+1,col,n,solArr)){ // We are moving forward to find forward path by taking previous assumption
+            return true; // If mouse is able to reach out then we return true
         }
+        // A mouse can travel in two directions only either forward or downward
+        // Here we are checking for downward path
         if(travel(arr,row,col+1,n,solArr)){
             return true;
         }
-        solArr[row][col] = 0;
+        // BackTracking
+        solArr[row][col] = 0; // Our assumption was wrong and mouse cannot escape out by following that path so we backtrack and reset our changes
         return false;
     }
 
+    // After going through all the conditions if it is not able to escape out then unfortunately we cannot help him and he is trapped forever
     return false;
     
 }
+
+
 void display(int** arr,int n){
     for(int i=0;i<n;i++){
         for(int j=0;j<n;j++){
